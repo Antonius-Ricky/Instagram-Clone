4b26f7371a579ec361ee4260e1c57bdfe01823d4
Antonius-Ricky <antoniusrickyigs@gmail.com>
2023-11-11 12:11:42 +0700

    11/11

--- application/routes.py
+++ application/routes.py
@@ -41,8 +41,8 @@ def logout():
 @login_required
 def profile(username):
     posts = current_user.posts
-    
-    return render_template('profile.html', title=f'{current_user.fullname} Profile', posts=posts)
+    reverse_posts = posts[::-1]
+    return render_template('profile.html', title=f'{current_user.fullname} Profile', posts=reverse_posts)
 
 @app.route('/', methods=('GET', 'POST'))
 @login_required
@@ -94,10 +94,29 @@ def forgotpassword():
     form = ForgotPasswordForm()
     return render_template('forgot_password.html', title='ForgotPassword', form=form)
 
-@app.route('/editprofile')
+@app.route('/editprofile',methods=['GET', 'POST'])
 def editprofile():
     form = EditProfileForm()
-    return render_template('profile_edit.html', title='Edit Profile', form=form)
+
+    if form.validate_on_submit():
+        user = User.query.get(current_user.id)
+        if form.username.data != user.username:
+            user.username = form.username.data
+        user.fullname = form.fullname.data
+        user.bio = form.bio.data
+
+        if form.profile_pic.data:
+            pass
+
+        db.session.commit()
+        flash('Profile updated', 'success')
+        return redirect(url_for('profile', username=current_user.username))
+    
+    form.username.data = current_user.username
+    form.fullname.data = current_user.fullname
+    form.bio.data = current_user.bio
+    
+    return render_template('profile_edit.html', title=f'Edit {current_user.username} Profile', form=form)
 
 @app.route('/resetpassword')
 def resetpassword():
